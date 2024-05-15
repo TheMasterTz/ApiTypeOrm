7313775bd46f3f9d364cc81559347a43bd169875
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2024-05-15 15:05:32 -0500

    feat: Add User model with columns for id, name, email, password, created_at, and updated_at

---
+++ src/models/User.model.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'users' })
+export class Users {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at!: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP'
+  })
+  updated_at!: Date;
+}
